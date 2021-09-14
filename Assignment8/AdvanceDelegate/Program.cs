c88555b5cfb45cf441bbd1adf32790d12b40098b
Salman7905 <salmanahmed9621@bbdu.ac.in>
2021-09-14 12:00:14 +0530

    Delegate and SwitchCase Program

---
+++ Assignment8/AdvanceDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AdvanceDeligate.Model; 
+delegate void Cal(double x,double y);
+namespace AdvanceDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TypeOfOperation obj;
+            Console.WriteLine("\n _______Calculator_______ \n Enter first Number :- ");
+            double a=Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("\n Enter the second number :- ");
+            double b=Convert.ToDouble(Console.ReadLine());
+            
+            obj=new TypeOfOperation(a,b);
+
+            int ch =0;
+            Console.WriteLine("1) Delegate Type \n2) Anonymous Type \n3) Lamda Function Type  \n4) Exit to main menu ");
+            while(ch!=4)
+            {  
+                Console.WriteLine("Enter your Choice :-");
+                ch=Convert.ToInt32(Console.ReadLine());
+                switch(ch)
+                {
+                    case 1:
+                        obj.delegateType();
+                        break;
+                    case 2:
+                        obj.anonymousType();
+                        break;
+                    case 3:
+                        obj.lamdaType();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            } 
+        }
+      }
+    }
---
+++ Assignment8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchCasePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch = 0;
+            while(ch!=4)
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine("1.) Add Employee\n2.) Update Employee\n3.) Delete Employee\n4.) Exit");
+                Console.WriteLine("Please enter your choice :)");
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch(ch)
+                {
+                    case 1:
+                           Console.WriteLine("Employee added successfully...");
+                           break;
+                    case 2: 
+                           Console.WriteLine("Employee deatils updated successfully...");
+                           break;
+                    case 3:
+                          Console.WriteLine("Employee deleted successfully...");
+                          break;
+                    case 4:
+                          Console.WriteLine("Exit successfully");
+                          break;
+                    default:
+                          Console.WriteLine("Invalid choice!");
+                          break;
+                }
+            }
+        }
+    }
+}
