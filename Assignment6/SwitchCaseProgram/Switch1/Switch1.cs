2684dbda257f548e70e7f5d23f12b5cb3f6e2f52
Salman7905 <salmanahmed9621@bbdu.ac.in>
2021-09-14 11:58:21 +0530

    SwitchCaseProgram

---
+++ Assignment6/SwitchCaseProgram/Switch1/Switch1.cs
@@ -0,0 +1,36 @@
+using System;
+namespace SwitchCaseProgram.Switch1
+{
+    public class Switch1
+    {
+       /*   Console.WriteLine("Please enter 1 for sunday , 2 for monday and so on...");
+          Console.WriteLine("Please enter Day number to print the day");
+          int Num=Convert.ToInt32(Console.ReadLine());           // ToInt32 for int and ToInt64 for long
+          switch(Num)
+          {
+              case 1:
+                   Console.WriteLine("Sunday");
+                   break;
+             case 2:
+                   Console.WriteLine("Monday");
+                   break;
+            case 3:
+                   Console.WriteLine("Tuesday");
+                   break;
+            case 4:
+                   Console.WriteLine("Wednesday");
+                   break;
+            case 5:
+                   Console.WriteLine("Thursday");
+                   break;
+            case 6:
+                   Console.WriteLine("Friday");
+                   break;
+            case 7:
+                   Console.WriteLine("Saturday");
+                   break;
+            default:
+                   Console.WriteLine("Invalid Input");
+                   break; */
+          }
+    }
