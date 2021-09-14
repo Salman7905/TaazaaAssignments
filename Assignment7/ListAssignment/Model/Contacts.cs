15556e74399a665765e9de6f3138a35a39d34e9e
Salman7905 <salmanahmed9621@bbdu.ac.in>
2021-09-14 11:59:12 +0530

    ListAssignments

---
+++ Assignment7/ListAssignment/Model/Contacts.cs
@@ -0,0 +1,10 @@
+namespace ListAssignment.Model
+{
+    public class Contacts
+    {
+        public int cId{get;set;}
+        public string cName{get;set;}
+        public string cMail{get;set;}
+        
+    }
+}
---
+++ Assignment7/ListAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ListAssignment.Model;
+namespace ListAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         List<Contacts> list = new List<Contacts>();
+         list.Add(new Contacts{
+           cId=101,
+           cName="Mike",
+           cMail="hi@gmail.com"
+         });
+         list.Add(new Contacts{
+             cId=102,
+             cName="Chris",
+             cMail="hello@gmail.com"
+         });
+
+            list.Add(new Contacts{
+             cId=103,
+             cName="Sam",
+             cMail="by@gmail.com"
+         });
+         
+         // updating
+         int tgt =3;
+         list[tgt].cName="Robert";
+         list[tgt].cMail="rbt@gmail.com";
+         Console.WriteLine("Updated Record "+list[tgt].cName +" "+list[tgt].cMail);
+         
+         //Deleting record
+         int t=1;
+         list[t].Remove(new Contacts{
+             list.c
+         });
+         // Showing all records
+         /* int count = list.Count;
+         for(int i=0;i<count;i++)
+         {
+             Console.WriteLine("ID => "+list[i].cId+"\nName =>"+list[i].cName+"\nMail => "+list[i].cMail);
+             Console.WriteLine("----------");
+         } */
+         }
+    }
+}
